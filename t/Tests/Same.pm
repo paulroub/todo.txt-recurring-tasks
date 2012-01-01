1a7c6e5f4a1ee6babbc36fef1a2ed1d5cca624c4
Paul Roub <paul@roub.net>
2011-12-31 19:36:40 -0500

    task matching

---
+++ t/Tests/Same.pm
@@ -0,0 +1,54 @@
+package Tests::Match;
+
+use Test::Class;
+use base qw(Test::Class);
+use Test::More;
+use Data::Dumper;
+
+use Todotxt::Recur;
+
+sub same
+{
+    my ($recurText, $taskText) = @_;
+
+    my $rt = Todotxt::Recur->new($recurText);
+
+    ok($rt->sameTask($taskText), "'$recurText' should match '$taskText'");
+}
+
+sub different
+{
+    my ($recurText, $taskText) = @_;
+
+    my $rt = Todotxt::Recur->new($recurText);
+
+    ok(! $rt->sameTask($taskText), "'$recurText' should not match '$taskText'");
+}
+
+sub test_trivial : Test(2) {
+    same("sunday: do a thing", "do a thing");
+    different("sunday: do a thing", "do another thing");
+};
+
+sub test_ignoreContextInTask : Test {
+    same("sunday: do a thing", 'do a thing @home');
+};
+
+sub test_ignoreDiffContexts : Test {
+    same('sunday: do a thing @work', 'do a thing @home');
+};
+
+sub test_ignoreProject : Test {
+    same("sunday: do a thing", 'do a thing +skynet');
+};
+
+sub test_ignoreDiffProjects : Test {
+    same('sunday: do a thing +deathstar', 'do a thing +skynet');
+};
+
+sub test_ignoreMultiple: Test(2) {
+    same('sunday: do a thing +deathstar +skynet @home @road', 'do a thing +skynet +elsewhere @home @work');
+    different('sunday: do a thing +deathstar +skynet @home @road', 'do some thing +skynet +elsewhere @home @work');
+};
+
+1;
---
+++ t/same.t
@@ -0,0 +1,4 @@
+#! /usr/bin/perl
+use Tests::Same;
+
+Test::Class->runtests();
